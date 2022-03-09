8b85b9cf94aff06d628d3a9d6d63c1aa700c7e57
nonokEE <shshrdl@naver.com>
2022-03-09 23:38:06 +0900

    17626 pass

--- test.py
+++ test.py
@@ -1,15 +1,5 @@
 import sys
 ip = sys.stdin.readline
 
-def quicksort(l):
-    if len(l) <= 1: return l
-    pivot = l[len(l)//2]
-    lt, gt = [], []
-    for item in l:
-        if   item < pivot: lt.append(item)
-        elif item > pivot: gt.append(item)
-    return quicksort(lt) + [pivot] + quicksort(gt)
-
-a = ['ac', 'dc', 'fb', 'a']
-quicksort(a)
-print(a)
+n = 25
+print(n**(1/2) == 5)
