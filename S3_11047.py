a60ff526695dd3920bb4b2ccb406ec6cb6f63744
nonokEE <shshrdl@naver.com>
2022-03-22 01:02:45 +0900

    11047: 2nd

--- S3_11047.py
+++ S3_11047.py
@@ -11,9 +11,8 @@ res = 0
 for i in range(N):
     if coins[i] > K: continue
     else:
-        while K >= coins[i]:
-            K -= coins[i]
-            res += 1
+        res += (K//coins[i])
+        K = K%coins[i]
     if K == 0: break
 
 print(res)
@@ -27,4 +26,8 @@ N K 입력 이후
 --1트-- : 시간초과
 그냥 제일 큰거부터 쓰면 되는거 아님? 아님 DP인가?
 일단 해보고 아니면 DP임.
+
+--2트--: 성공ㅋㅋ
+DP 하기전에, 저거 수식이 좀 무식하지 않나.
+%랑 //써도 될 것 같은데 while 돌림?
 '''
