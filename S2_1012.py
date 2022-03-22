8fb20a72803c627aba940c10005f7aa68e9989aa
nonokEE <shshrdl@naver.com>
2022-03-22 23:14:23 +0900

    1012: 3rd clear

--- S2_1012.py
+++ S2_1012.py
@@ -3,27 +3,29 @@ ip = sys.stdin.readline
 
 T = int(ip())
 
-def DFS(curx, cury, farm, visited, cabbage):
-    visited.append([curx, cury])
-    cabbage.remove([curx, cury])
-    # LEFT
-    if curx > 0:
-        if ([curx-1, cury] not in visited) and farm[curx-1][cury]: DFS(curx-1, cury, farm, visited, cabbage)
-    # RIGHT
-    if curx < len(farm)-1:
-        if ([curx+1, cury] not in visited) and farm[curx+1][cury]:DFS(curx+1, cury, farm, visited, cabbage)
-    # UP
-    if cury > 0:
-        if ([curx, cury-1] not in visited) and farm[curx][cury-1]:DFS(curx, cury-1, farm, visited, cabbage)
-    # DOWN
-    if cury < len(farm[0])-1:
-        if ([curx, cury+1] not in visited) and farm[curx][cury+1]:DFS(curx, cury+1, farm, visited, cabbage)
-
+def DFS(start, farm, cabbage):
+    visited = []
+    non_visited = [start]
+    while non_visited:
+        curx, cury = non_visited.pop()       
+        cabbage.remove([curx, cury])
+        visited.append([curx, cury])
+        # LEFT
+        if cury < len(farm[0])-1:
+            if ([curx, cury+1] not in visited + non_visited) and farm[curx][cury+1] :non_visited.append([curx, cury+1])
+        # RIGHT
+        if cury > 0:
+            if ([curx, cury-1] not in visited + non_visited) and farm[curx][cury-1]:non_visited.append([curx, cury-1])
+        # UP
+        if curx < len(farm)-1:
+            if ([curx+1, cury] not in visited + non_visited) and farm[curx+1][cury]:non_visited.append([curx+1, cury])
+        # DOWN
+        if curx > 0:
+            if ([curx-1, cury] not in visited + non_visited) and farm[curx-1][cury]: non_visited.append([curx-1, cury])
 
 for _ in range(T):
     M, N, K = map(int, ip().split())
     farm = [[0 for _ in range(N)] for _ in range(M)]
-    visited = []
     cabbage = []
     count = 0
 
@@ -33,7 +35,7 @@ for _ in range(T):
         cabbage.append([x,y])
 
     while cabbage:
-        DFS(cabbage[0][0], cabbage[0][1], farm, visited, cabbage)
+        DFS(cabbage[0], farm, cabbage)
         count+=1
 
     print(count)
@@ -61,10 +63,14 @@ for _ in range(T):
 재귀가 터져버림. DFS에서 일단 보내고 방문 여부 찾지 말고, 방문여부를 먼저 찾고 DFS 보낼지 말지 결정해야 함.
 어차피 DFS 시작을 무조건 배추땅에서 하니까, farm 여부도 미리 보고 보내자.
 
---3트-- :
+--3트-- : 재귀없이 하니 성공!
 탐색 방법을 재귀 안터지게 바꿔야 함. BFS로 구현해봐? 아니면 배추 심어지는 시점에서 인접리스트를 만들어야함.
 BFS냐 DFS냐는 별로 안중요한듯. 인접행렬/인접리스트의 시간복잡도가 다르대.
+
 인접행렬 = O(n^2)
 인접리스트 = O(n+E)
 인접리스트를 구현해보자.
+
+아니면 재귀를 안쓰고 스택으로 하는 방법도 있지 않나?
+애초에 지금 인접 행렬을 안쓰고 있음. 재귀 안쓰고 해결하는걸로 해보자.
 '''
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import sys
 ip = sys.stdin.readline
 
-a = [[1,2], [3,4], [5,6]]
-a.remove([1,2])
+a = [1,2,3,4]
+a.pop()
 print(a)
