f531e078c605f81eec4e76b537afd5294e3b9bc6
NonokEE <shshrdl@naver.com>
2023-04-27 18:38:38 +0900

    1107: try3- success

--- G5_1107.py
+++ G5_1107.py
@@ -1,113 +1,24 @@
 import sys
 ip = sys.stdin.readline
 
-n = ip().strip()
+n = int(ip())
 m = int(ip())
 broken = []
 if m: 
     broken = list(map(int, ip().split()))
 
-cur = '100'
-res = 0
+res = abs(n - 100) # +-만 가지고 이동하는 경우를 일단 최솟값으로 책정
 
-# 자릿 수 올리거나 내릴 때 10 되거나 -1 된거 정리
-def digit_organize(num:list):
-    # 10 정리
-    for i in range(len(num)-1, 0, -1):
-        if num[i] > 9:
-            num[i] -= 10
-            num[i-1] += 1
-    if num[0] > 9:
-        num[0] -= 10
-        num = [1] + num
-    # -1 정리
-    for i in range(len(num)-1, 0, -1):
-        if num[i] < 0:
-            num[i] += 10
-            num[i-1] -= 1
-    if num[0] == 0:
-        if len(num) > 1: num = num[1:]
-    return num
-
-# 고장난 부분 찾기. 없으면 None 반환
-def get_broken_point(num, broken:list):
-    for digit in range(len(num)):
-        if int(num[digit]) in broken:
-            return digit
-    else:
-        return None
-
-# 로직 #
-if n == '100':
-    res = 0
-elif not broken:
-    res = min(len(n), abs(int(n) - 100))
-elif len(broken) == 10:
-    res = abs(int(n) - 100)
-else:
-    INF = 99999999
-    candidate = [abs(int(n) - 100)]  #일단 어떤 경우에도 쌩으로 이동하는거랑은 비교해야됨
-
-    # 버튼 고장난 부분 검사
-    point = get_broken_point(n, broken)
-
-    if point == None: # 고장 해당 없다면 그냥 누르면 되고, 그게 웬만하면 int(n) - 100과 함께 최소의 경우가 될 것.
-        candidate.append(len(n))
-
-    # 눌러야 할 버튼이 고장난 경우
-    else:
-        ########
-        # 고장난 자리 올리기
-        higher = list(map(int,list(n))) #숫자 리스트로 변경
-
-        # 1. 아랫 자리들 가능한 작은 수로 변경
-        num = 0
-        while num in broken: num += 1
-
-        for i in range(point+1, len(higher)):
-            higher[i] = num
-
-        # 2. 바꿀 자리 포함 윗 자리 재점검
-        while (point != None):
-            higher[point] += 1 #바꿀 자리 바꾸기
-            higher = digit_organize(higher) 
-            point = get_broken_point(higher, broken)
+for channel in range(1000001):      #0번 채널부터 1,000,000번 채널까지
+    for digit in str(channel):      #문자열화 하고 고장났는지 확인
+        if int(digit) in broken:    #고장났으면 넘겨
+            break
+    else:                       #고장안났으면 그 채널의 클릭 수를 최소값으로.
+        val = len(str(channel)) + abs(channel - n)
+        res = min(res, val)
             
-            higher_integerized = int("".join(list(map(str, higher))))
-            count = higher_integerized - int(n) + len(higher)
-            if count > candidate[0]: #생으로 옮기는것보다 비효율적이게 되면 볼 필요도 없음
-                break
-        else:    
-            candidate.append(count)
-
-        ########
-        # 고장난 자리 내리기
-        lower = list(map(int,list(n)))
-        point = get_broken_point(lower, broken)
-
-        # 1. 아랫 자리들 가능한 큰 수로 변경
-        num = 9
-        while num in broken: num -= 1
-
-        for i in range(point+1, len(lower)):
-            lower[i] = num
-
-        # 2. 바꿀 자리 포함 윗 자리 재점검
-        while point != None:
-            lower[point] -= 1 #바꿀 자리 바꾸기
-            lower = digit_organize(lower) 
-            point = get_broken_point(lower, broken)
-
-            lower_integerized = int("".join(list(map(str, lower))))
-            count = int(n) - lower_integerized + len(lower)
-            if count > candidate[0]: 
-                break
-        else:    
-            candidate.append(count)    
-
-    res = min(candidate)
-
 print(res)
+
 
 
 ''' 리모컨
@@ -129,7 +40,10 @@ N채널로 이동하고 싶다 치고
 둘째 줄에 고장난 버튼 개수 M(0이상 10이하)
 고장난 버튼이 있는 경우에만 세번째 줄 들어오고, 어떤 버튼 고장났는지 주어짐. 중복은 X
 
-
+--2트--
+아예 방법을 다르게 생각해야 한다.
+저런 스마~트한 방법으로 하려고 하지 말고, 냥꾼처럼 무식하게 찾자. 브루트포스잖아
+틀리면 서치 좀 똑똑하게 해보고, 그래도 안되면 답지 본다
 
 --1트--: 틀렸습니다
 오랜만에 무식한 문제가 나온 것 같습니다. 탐색 알고리즘 아닌거같음.
