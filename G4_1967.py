e3b5242b455f0bc7a90580775cd3aeb951a40fec
NonokEE <shshrdl@naver.com>
2023-06-07 19:43:30 +0900

    1967: t2 - Time over

--- G4_1967.py
+++ G4_1967.py
@@ -2,32 +2,37 @@ import sys
 ip = sys.stdin.readline
 
 N = int(ip())
-
-INF = sys.maxsize
-adj_mat = [[INF] * N for _ in range(N)]
+adj_list = {i:{} for i in range(N)}
 
 for _ in range(N-1):
-    v1, v2, w = map(int, ip().split())
-    v1, v2 = v1-1, v2-1
-    adj_mat[v1][v2] = w
-    adj_mat[v2][v1] = w
+    parent, child, weight = map(int, ip().split())
+    parent -= 1
+    child -= 1
+    adj_list[parent][child] = weight
+    adj_list[child][parent] = weight
 
-for i in range(N):
-    adj_mat[i][i] = 0
+def DFS (s):
+    stack = [(s, 0)]
+    visited = [False for _ in range(N)]
 
-for mid in range(N):
-    for s in range(N):
-        for e in range(N):
-            if adj_mat[s][e] > adj_mat[s][mid] + adj_mat[mid][e]:
-                adj_mat[s][e] = adj_mat[s][mid] + adj_mat[mid][e]
+    res = 0
+    while stack:
+        cur, val = stack.pop()
+        
+        if val > res:
+            res = val
 
-res = []
-for line in adj_mat:
-    res.append(max(line))
-print(max(res))
+        if not visited[cur]:
+            visited[cur] = True
+            for child in adj_list[cur].keys():
+                if not visited[child]:
+                    stack.append((child, val + adj_list[cur][child]))
 
+    return res
 
-#dijkstra
+vals = [DFS(i) for i in range(N)]
+print(max(vals))
+
 
 ''' 트리의 지름
 시간 2초 메모리 128MB
@@ -42,7 +47,13 @@ print(max(res))
 - 출력-
 트리의 지름을 출력
 
---1트--: 결과
+--2트--:
+그래 너무 많아.. 메모리 안걸려도 어차피 시간에서 걸렸을듯
+각 노드에 다 돌려서 가장 긴거 찾는건 똑같은데, 알고리즘 자체의 필요조건을 이해하고 있느냐의 차이
+플로이드워셜은 너무 자원이 많이 들고, 다익스트라는 너무 오래 걸림.
+DFS를 각 노드로 돌려주는게 낫다.
+
+--1트--: 메모리 초과
 트리라서 DFS인 척 하는데, 다익스트라로 해결하면 그만 아닌가 싶음.
 루트에서 다익스트라 돌린 다음에 1,2번째로 긴거 출력하면 되는거 아닌가?
 ㄴㄴ 그게 쫙 펴지는 경우는 아니라서..
